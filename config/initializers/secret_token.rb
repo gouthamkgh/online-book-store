a1576b6ebdfde0bc66b9fc887a38d38552ddcb8a
Goutham Harsha <gouthamkgh@gmail.com>
2012-05-27 02:30:09 +0530

    First one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DepotApp::Application.config.secret_token = 'a93dce19270b338e144a37ba46401bcbd6cb562ad287d2d9d878cb38cb7df2ebf1e13c8804d830b6dde8c3fb0ec8793e813172cff18098d536c4982a2629ba50'
---
+++ db/seeds.rb
@@ -0,0 +1,81 @@
+Product.delete_all
+Product.create(:title => 'The New Social Learning',
+  :description => 
+    %{<p>
+        <em>The New Social Learning</em> examines the use of social media for learning, explores a specific social media approach in depth in each chapter and elaborates on how it leads to tacit and explicit learning. Concepts are punctuated with case studies of companies using social media effectively.
+      </p>},
+  :image_url =>   '/images/3.jpg',    
+  :price => 27.50)
+# . . .
+
+Product.create(:title => 'Web Design for Developers',
+  :description => 
+    %{<p>
+        <em>Web Design for Developers</em> will show you how to make your
+        web-based application look professionally designed. We'll help you
+        learn how to pick the right colors and fonts, avoid costly interface
+        and accessibility mistakes -- your application will really come alive.
+        We'll also walk you through some common Photoshop and CSS techniques
+        and work through a web site redesign, taking a new design from concept
+        all the way to implementation.
+      </p>},
+  :image_url =>   '/images/wd4d.jpg',    
+  :price => 42.95)
+# . . .
+
+Product.create(:title => 'Web Design for Developers',
+  :description => 
+    %{<p>
+        <em>Web Design for Developers</em> will show you how to make your
+        web-based application look professionally designed. We'll help you
+        learn how to pick the right colors and fonts, avoid costly interface
+        and accessibility mistakes -- your application will really come alive.
+        We'll also walk you through some common Photoshop and CSS techniques
+        and work through a web site redesign, taking a new design from concept
+        all the way to implementation.
+      </p>},
+  :image_url =>   '/images/wd4d.jpg',    
+  :price => 42.95)
+# . . .
+
+
+Product.create(:title => 'Web Design for Developers',
+  :description => 
+    %{<p>
+        <em>Web Design for Developers</em> will show you how to make your
+        web-based application look professionally designed. We'll help you
+        learn how to pick the right colors and fonts, avoid costly interface
+        and accessibility mistakes -- your application will really come alive.
+        We'll also walk you through some common Photoshop and CSS techniques
+        and work through a web site redesign, taking a new design from concept
+        all the way to implementation.
+      </p>},
+  :image_url =>   '/images/wd4d.jpg',    
+  :price => 42.95)
+# . . .
+Product.create(:title => 'Programming Ruby 1.9',
+  :description =>
+    %{<p>
+        Ruby is the fastest growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbox.
+      </p>},
+  :image_url => '/images/ruby.jpg',
+  :price => 49.50)
+# . . .
+
+Product.create(:title => 'Rails Test Prescriptions',
+  :description => 
+    %{<p>
+        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
+        Rails applications, covering Test-Driven Development from both a
+        theoretical perspective (why to test) and from a practical perspective
+        (how to test effectively). It covers the core Rails testing tools and
+        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
+        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+      </p>},
+  :image_url => '/images/rtp.jpg',
+  :price => 43.75)
+# . . .
+
+
